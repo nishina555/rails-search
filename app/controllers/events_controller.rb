ab4bc19e86357efed6d1518e57a10598a90ccf9c
nishina <n.toshi.1.014@gmail.com>
2016-11-27 17:58:12 +0900

    Add search result page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,12 @@
 class EventsController < ApplicationController
   before_filter :set_search
   def index
+  end
+
+  def search
     @events = @q.result(distinct: true)
   end
+
   private
   def set_search
     @q = Event.search(params[:q])
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events
+  resources :events do
+    collection do
+      get :search
+    end
+  end
 end
